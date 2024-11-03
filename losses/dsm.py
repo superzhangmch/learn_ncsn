ee317c2f0e4ecb642e65fe9fce1e05345cd10461
Zhang Miaochang <superzhangmch@163.com>
2024-11-03 20:52:40 +0800

    Update dsm.py

--- losses/dsm.py
+++ losses/dsm.py
@@ -4,7 +4,7 @@ import torch.autograd as autograd
 
 def dsm(energy_net, samples, sigma=1):
     samples.requires_grad_(True)
-    vector = torch.randn_like(samples) * sigma
+    vector = torch.randn_like(samples) * sigma # 本来denoising score matching就需要对数据加噪（加一个比较小的噪，然后在这上面做 score matching）
     perturbed_inputs = samples + vector
     logp = -energy_net(perturbed_inputs)
     dlogp = sigma ** 2 * autograd.grad(logp.sum(), perturbed_inputs, create_graph=True)[0]
@@ -27,7 +27,8 @@ def dsm_score_estimation(scorenet, samples, sigma=0.01):
 
 
 def anneal_dsm_score_estimation(scorenet, samples, labels, sigmas, anneal_power=2.):
-    used_sigmas = sigmas[labels].view(samples.shape[0], *([1] * len(samples.shape[1:])))
+    used_sigmas = sigmas[labels].view(samples.shape[0], *([1] * len(samples.shape[1:]))) # 本来denoising score matching 是选定一个noise然后都用它。但在该代码对应的paper的框架下，要加不同level 的noise，并在不同noise上训。
+                                                                                         # 相当于训了多个denoising score matching 
     perturbed_samples = samples + torch.randn_like(samples) * used_sigmas
     target = - 1 / (used_sigmas ** 2) * (perturbed_samples - samples)
     scores = scorenet(perturbed_samples, labels)
